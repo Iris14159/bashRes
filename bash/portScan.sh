984809a5c73efaed81f11becbd92093305e83f2e
celiphcephei <ampheriz0@gmail.com>
2024-02-11 23:04:39 +0000

    bortScan

--- bash/portScan.sh
+++ bash/portScan.sh
@@ -8,6 +8,12 @@ function ctrl_c(){
 # Ctrl+C
 trap ctrl_c INT
 
+# Ocultar cursor
+tput civis
+
 for port in $(seq 1 65535); do
   (echo '' > /dev/tcp/127.0.0.1/$port) 2>/dev/null && echo "[+] Port $port - OPEN" & # Hilo de ejecucion
 done; wait # Espera a que los hilos finalicen
+
+# Mostrar cursor
+tput cnorm
